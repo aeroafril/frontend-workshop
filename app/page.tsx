dda225ccf3bf5fd5d89b1de5e1f3fd64463eb613
aero afril <semarg53@gmail.com>
2026-01-30 15:58:21 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home(){
   return(
     <main className="min-h-screen bg-gray-100 p-6 flex items-center justify-center">
       <div className="bg-white p-6 rounded-xl shadow max-w-md w-full">
-        <h1 className="text-3xl font-bold text-ble-600 mb-4">Tailwind Aktif</h1>
+        <h1 className="text-3xl font-bold text-blue-600 mb-4">Provinsi Indonesia</h1>
         <p className="text-gray-600 mb-6">Klik di bawah untuk melihat daftar provinsi indonesia</p>
         <Link href="/provinces" className="inline-flex items-center justify-center px-5 py-3 rounded-lg bg-blue-600 text-white font-medium hover:bg-blue-700 transition">Lihat data Provinsi</Link>
       </div>
